f928f5f90b570baa7f7e74e9a2fc6a81ed4b6f2b
Tothko <43235034+Tothko@users.noreply.github.com>
2019-11-24 14:14:02 +0100

    Initial commit

---
+++ SQLData/DbInitializer.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace SQLData
+{
+    public class Class1
+    {
+    }
+}
---
+++ SQLData/FitLineShopContext.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SQLData
+{
+    class Class1
+    {
+    }
+}
